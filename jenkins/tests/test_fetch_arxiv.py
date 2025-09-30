8de7eae702cd79bf5a702565ba28da2d9b7a3e9c
SeverineLEGUY <sleca@yahoo.fr>
2025-09-30 08:59:35 +0200

    Stabilisation Pytest dans Jenkins

--- jenkins/tests/test_fetch_arxiv.py
+++ jenkins/tests/test_fetch_arxiv.py
@@ -7,7 +7,7 @@ from jenkins.tests.fetch_arxiv_unitaire import fetch_arxiv, upload_to_s3
 
 
 # Exemple de configuration
-TEST_QUERY = "machine learning"
+TEST_QUERY = "machine learning"cd 
 MAX_RESULTS = 5
 TEST_OUTPUT_FILE = "data/output_data.csv"
 
